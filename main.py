f3e365f2719cd4a15ae01014ce3777ad929d999e
Ay-De <91670696+Ay-De@users.noreply.github.com>
2023-01-16 14:48:02 +0100

    Automatic Webdriver download for Edge added

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
+import os
+from tqdm import tqdm
 import requests
+from zipfile import ZipFile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.edge.options import Options
@@ -6,10 +9,34 @@ import xml.etree.ElementTree as ET
 import re
 
 
+class SetupWebdriver:
+    def __init__(self):
+        self.chromium_dir = 'C:\Program Files (x86)\Microsoft\Edge\Application'
+        self.version = self.get_chromium_version(self.chromium_dir)
+        self.download_webdriver(self.version)
+        self.unzip
+        
+    def get_chromium_version(self, chromium_dir):
+        v = [d.path.split("\\")[-1] for d in os.scandir(chromium_dir) if d.is_dir() and d.path.split("\\")[-1].split(".")[0].isnumeric()]
+        return sorted(v, reverse=True)[0]
+
+    def download_webdriver(self, version):
+        self.url = 'https://msedgedriver.azureedge.net/' + version + '/edgedriver_win64.zip'
+        response = requests.get(self.url, stream=True)
+
+        with open('edgedriver_win64.zip', 'wb') as f:
+            for data in tqdm(response.iter_content()):
+                f.write(data)
+
+    def unzip(self):
+        with open(ZipFile('edgedriver_win64.zip', 'r')) as zip:
+            zip.extract('msedgedriver.exe')
+
+
 class HeadlessBrowser:
     
     def __enter__(self):
-
+        
         class Downloader:
             def __init__(self):
 
@@ -27,10 +54,7 @@ class HeadlessBrowser:
 
             def _get_chapters(self, chapters_url):
                 self.webdriver.get(chapters_url)
-                #x = requests.get(chapters_url, headers={'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.50'})
-                #self.xmldoc = etree.fromstring(x.text)
 
-                #self.parser = etree.XMLParser(recover=True)
                 xx = self.webdriver.find_element(by=By.TAG_NAME, value='body').text
                 namespaces = {'xmlns': "http://www.w3.org/2005/Atom"}
                 self.tree = ET.fromstring(xx)
@@ -55,6 +79,8 @@ class HeadlessBrowser:
 
 
 def main():
+
+    SetupWebdriver()
 
     with HeadlessBrowser() as headless:
 
