cfc2bafeb9de5364c7267bacaee353bcc79b1b3b
Ay-De <91670696+Ay-De@users.noreply.github.com>
2023-02-02 00:48:45 +0100

    Chapter URL scrapper again rewritten and added filter for unpublished chapters

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ from selenium.common.exceptions import TimeoutException, StaleElementReferenceEx
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.edge.options import Options
-import xml.etree.ElementTree as ET
 import re
 
 from modules.webdriver import setup_webdriver
@@ -21,37 +20,59 @@ class HeadlessBrowser:
 
             def __init__(self):
 
-                self.manga_url = \
-                    'https://tonarinoyj.jp/episode/316190247133138894'
-                # 'https://tonarinoyj.jp/atom/series/13932016480028984490'
+                self.manga_url = 'https://tonarinoyj.jp/episode/316190247048704205'
+                self.tonari_url = 'https://tonarinoyj.jp/episode/'
                 self.download_location = 'D:\OPM\\'
 
                 self.chapter_links = {}
 
                 self.options = Options()
-                # self.options.add_argument('headless')
+                #self.options.add_argument('headless')
                 self.options.add_argument('window-size=1920x1080')
                 self.options.add_argument('disable-extensions')
                 self.webdriver = webdriver.Edge(options=self.options)
 
+                self.webdriver.set_network_conditions(
+                        offline=False,
+                        latency=1,  # additional latency (ms)
+                        download_throughput= 28*500*1024,  # maximal throughput
+                        upload_throughput=500*1024)  # maximal throughput
+
                 self._get_chapters_links()
 
-                print('********\nFound {} chapters. Select chapters for download. Options:'.format(
-                    len(self.chapter_links.keys())))
+                print(f'********\nFound {len(self.chapter_links.keys())} '
+                      f'chapters. Select chapters for download.\nOptions:')
 
                 while True:
                     self._download_selection = input(
                         'Chapter number, range (ex 1-3), all or latest')
 
-                    try:
-                        if self._download_selection.lower() == 'latest':
-                            print(sorted(list(self.chapter_links.keys()))[-1])
+                    if self._download_selection.lower() == 'latest':
+                        print(list(self.chapter_links.keys()))
+                        #break
 
-                        elif self._download_selection.lower() == 'all':
-                            print(len(list(self.chapter_links.keys())))
+                    elif self._download_selection.lower() == 'all':
+                        print(len(list(self.chapter_links.keys())))
+                        #break
 
-                    except Exception as e:
-                        print(e)
+                    else:
+                        try:
+                            if int(self._download_selection):
+                                print(int(self._download_selection))
+                                #break
+
+                        except ValueError:
+                            try:
+                                self._chap_range_lower, \
+                                self._chap_range_upper = self._download_selection.split('-')
+
+                                print('Chapters from {} to {} will be downloaded.'.format(
+                                    self._chap_range_lower, self._chap_range_upper))
+                                #break
+
+                            except ValueError:
+                                print('Invalid input. Example input for range: 1-5')
+
 
                 self._get_image_links(title[0], url)
 
@@ -60,50 +81,69 @@ class HeadlessBrowser:
                 self.webdriver.get(self.manga_url)
                 wait = WebDriverWait(self.webdriver, 10)
 
-                wait.until(EC.visibility_of_element_located((By.XPATH,
-                                                       '//div[@class="series-contents"]//div['
-                                                       '@class="js-readable-product-list"]')))
+                # wait.until(EC.visibility_of_element_located((By.XPATH,
+                #                                        '//div[@class="series-contents"]//div['
+                #                                        '@class="js-readable-product-list"]')))
 
-                _chap_list_cont = self.webdriver.find_element(By.XPATH, '//div[@class="series-contents"]//div['
-                                                       '@class="js-readable-product-list"]')
+               # _chap_list_cont = self.webdriver.find_element(By.XPATH, '//div[@class="series-contents"]//div['
+                #                                       '@class="js-readable-product-list"]')
+
+                _chap_list_cont = self.webdriver.find_element(By.XPATH,
+                                                              '//button[@class="js-read-more-button"]')
 
                 self.webdriver.execute_script('return arguments[0].scrollIntoView();',
                                               _chap_list_cont)
+
+                _chapter_container_loaded = wait.until(EC.visibility_of_element_located((By.XPATH,
+                                                                 '//span[@class="loading-text"]')))
+                wait.until(lambda x: 'hidden' in _chapter_container_loaded.get_attribute('class'))
 
                 while True:
 
                     try:
                         wait.until(EC.visibility_of_element_located(
-                                    (By.XPATH, '//section[@class="read-more-container"]'
-                                                '//button[@class="js-read-more-button"]'))).click()
+                                    (By.XPATH, '//button[@class="js-read-more-button"]'))).click()
 
                     except (TimeoutException, StaleElementReferenceException):
                         break
 
-                chap_elems = self.webdriver.find_elements(
-                                    By.XPATH, '//a[@class="series-episode-list-container"]')
+                _chapters_available = self.webdriver.find_elements(By.XPATH,
+                        '//*[@class=" episode" and not(contains(@class, "private episode"))] | '
+                        '//*[@class="episode current-readable-product"]')
 
-                for c in chap_elems:
-                    print(re.findall(r".*?\[[^\d]*(\d+)[^\d]*\].*", c.text))
-                    print(c.get_attribute('href') + "\n")
+                _chapters_private = self.webdriver.find_elements(By.XPATH,
+                        '//*[@class="private episode"]')
 
+                regx = re.compile(r".*?\[[^\d]*(\d+)[^\d]*\].*")
 
+                for c in _chapters_available:
+                    c_num = regx.findall(c.text)
+                    c_url = self.tonari_url + c.get_attribute('data-id')
 
-                # self.tree = self.webdriver.find_element(by=By.TAG_NAME,
-                #                                         value='body').text
-                #
-                # self.tree = ET.fromstring(self.tree)
-                #
-                # for e in self.tree.findall(
-                #         '{http://www.w3.org/2005/Atom}entry'):
-                #     title = e.find('{http://www.w3.org/2005/Atom}title').text
-                #     title = re.findall(r"[0-9]+", title)
-                #     url = e.find('{http://www.w3.org/2005/Atom}link').get('href')
+                    if c_num:
+                        self.chapter_links.update({int(c_num[0]): c_url})
 
-                if title:
-                    self.chapter_links.update({title[0]: url})
+                _all_chapters = sorted(list(self.chapter_links.keys()))
 
-                print("########### chapters" + len(list(self.chapter_links.keys())))
+                _chapter_diffs = [x - y for (x, y) in zip(_all_chapters[1:], _all_chapters)]
+
+                _idx_skipped_chapters = [i for i, v in enumerate(_chapter_diffs) if v > 1]
+
+                if len(_idx_skipped_chapters) > 1:
+
+                    #Add here logic to find multiple gaps of unpublished chapters
+                    print(f'Note: Chapters {_all_chapters[_idx_skipped_chapters[0]] + 1} to '
+                          f'{_all_chapters[_idx_skipped_chapters[0]]} are missing. '
+                          f'Probably unpublished.')
+
+                elif len(_idx_skipped_chapters) == 1:
+                    if int(_idx_skipped_chapters[0]) > 1:
+                        print(f'Note: Chapters {_all_chapters[_idx_skipped_chapters[0]] + 1} to '
+                              f'{_all_chapters[_idx_skipped_chapters[0]]} are missing. '
+                              f'Probably unpublished.')
+                    else:
+                        print(f'Note: Chapter {_all_chapters[_idx_skipped_chapters[0]]} is missing. '
+                              f'Probably unpublished.')
 
             def _get_image_links(self, chapter_num, chapter_url):
 
